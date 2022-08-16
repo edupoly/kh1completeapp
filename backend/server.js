360c2d903326f581797a554bd421cedefffe78d1
edupoly <info.edupoly@gmail.com>
2022-08-16 13:32:35 +0530

    food

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express();
+var cors = require('cors')
+var foodItems = require('./food')
+app.use(cors())
+
+app.get('/foodItems',function(req,res){
+    res.send(foodItems)
+})
+
+app.listen(5500);
