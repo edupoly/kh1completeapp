0d9ce21aef5a1c643e2c822e9eacb7f877f49dae
edupoly <info.edupoly@gmail.com>
2022-08-17 13:32:59 +0530

    sessions

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,16 @@ var app = express();
 var cors = require('cors')
 var foodItems = require('./food')
 app.use(cors())
-
+app.use(function(req,res,next){
+   console.log("simple middleware called")
+   setTimeout(()=>{
+    next();
+   },1000)
+})
+app.use(function(req,res,next){
+    console.log("second middleware called")
+    next()
+})
 app.get('/foodItems',function(req,res){
     res.send(foodItems)
 })
--- uiserver/src/App.js
+++ uiserver/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Restaurants from './Restaurants';
 
 function App() {
+  function abc(){
+    fetch("http://localhost:5500/something")
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+    })
+  }
   return (
     <div>
+     <button onClick={abc}>Example session</button>
+
      <Restaurants></Restaurants>
     </div>
   );
